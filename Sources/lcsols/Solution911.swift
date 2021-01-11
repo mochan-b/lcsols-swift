78aaa267a547f3c55c4a55f5cd67d14a747d9449
Mochan Shrestha <git@mochan.info>
2021-01-11 04:50:20 +0000

    Solution 911 clean up

--- Sources/lcsols/Solution911.swift
+++ Sources/lcsols/Solution911.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 public class TopVotedCandidate {
-    var timevals: [Int];
+    var timeVals: [Int];
     var winners: [Int];
 
     public init(_ persons: [Int], _ times: [Int]) {
-        timevals = times;
+        timeVals = times;
         winners = [];
 
         var first: Int = 0;
@@ -13,17 +13,17 @@ public class TopVotedCandidate {
 
         var votes: [Int: Int] = [:];
         for i in 0...persons.count - 1 {
-            var person = persons[i];
+            let person = persons[i];
 
             if (votes[person] == nil) {
                 votes[person] = 0;
             }
 
-            var personvote = votes[person]! + 1;
-            votes[person] = personvote;
+            let personVote = votes[person]! + 1;
+            votes[person] = personVote;
 
-            if (personvote >= first) {
-                first = personvote;
+            if (personVote >= first) {
+                first = personVote;
                 firstname = person;
             }
 
@@ -32,16 +32,16 @@ public class TopVotedCandidate {
     }
 
     public func q(_ t: Int) -> Int {
-        var left = 0, right = timevals.count - 1, mid = 0;
+        var left = 0, right = timeVals.count - 1, mid = 0;
         var found = false;
 
         while (left <= right) {
             mid = (left + right) / 2;
 
-            if (timevals[mid] == t) {
+            if (timeVals[mid] == t) {
                 found = true;
                 break;
-            } else if (timevals[mid] < t) {
+            } else if (timeVals[mid] < t) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
--- Tests/lcsolsTests/Solution911Tests.swift
+++ Tests/lcsolsTests/Solution911Tests.swift
@@ -3,68 +3,68 @@ import XCTest
 
 @testable import lcsols
 
-final class Solution911Tests : XCTestCase {
+final class Solution911Tests: XCTestCase {
     func test1() {
-        let persons = [0,1,1,0,0,1,0]
-        let times = [0,5,10,15,20,25,30]
-        var topVotedCandidate = TopVotedCandidate(persons, times);
+        let persons = [0, 1, 1, 0, 0, 1, 0]
+        let times = [0, 5, 10, 15, 20, 25, 30]
+        let topVotedCandidate = TopVotedCandidate(persons, times);
 
-        var ts = [[3],[12],[25],[15],[24],[8]];
-        var ans = [0,1,1,0,0,1];
-        for i in 0...ts.count-1 {
-            var winner = topVotedCandidate.q(ts[i][0]);
+        let ts = [[3], [12], [25], [15], [24], [8]];
+        let ans = [0, 1, 1, 0, 0, 1];
+        for i in 0...ts.count - 1 {
+            let winner = topVotedCandidate.q(ts[i][0]);
             XCTAssertEqual(ans[i], winner);
         }
     }
 
     func test2() {
-        let persons = [0,1,2,2,1];
-        let times = [20,28,29,54,56];
-        var topVotedCandidate = TopVotedCandidate(persons, times);
+        let persons = [0, 1, 2, 2, 1];
+        let times = [20, 28, 29, 54, 56];
+        let topVotedCandidate = TopVotedCandidate(persons, times);
 
-        var ts = [[28],[53],[57],[29],[29],[28],[30],[20],[56],[55]];
-        var ans = [1,2,1,2,2,1,2,0,1,2];
-        for i in 0...ts.count-1 {
-            var winner = topVotedCandidate.q(ts[i][0]);
+        let ts = [[28], [53], [57], [29], [29], [28], [30], [20], [56], [55]];
+        let ans = [1, 2, 1, 2, 2, 1, 2, 0, 1, 2];
+        for i in 0...ts.count - 1 {
+            let winner = topVotedCandidate.q(ts[i][0]);
             XCTAssertEqual(ans[i], winner);
         }
     }
 
     func test3() {
-        let persons = [0,0,0,0,1];
-        let times = [0,6,39,52,75];
-        var topVotedCandidate = TopVotedCandidate(persons, times);
+        let persons = [0, 0, 0, 0, 1];
+        let times = [0, 6, 39, 52, 75];
+        let topVotedCandidate = TopVotedCandidate(persons, times);
 
-        let ts = [[45],[49],[59],[68],[42],[37],[99],[26],[78],[43]];
-        let ans = [0,0,0,0,0,0,0,0,0,0];
-        for i in 0...ts.count-1 {
-            var winner = topVotedCandidate.q(ts[i][0]);
+        let ts = [[45], [49], [59], [68], [42], [37], [99], [26], [78], [43]];
+        let ans = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        for i in 0...ts.count - 1 {
+            let winner = topVotedCandidate.q(ts[i][0]);
             XCTAssertEqual(ans[i], winner);
         }
     }
 
     func test4() {
-        var persons = [0,0,1,1,2];
-        var times = [0,67,69,74,87];
-        var topVotedCandidate = TopVotedCandidate(persons, times);
+        let persons = [0, 0, 1, 1, 2];
+        let times = [0, 67, 69, 74, 87];
+        let topVotedCandidate = TopVotedCandidate(persons, times);
 
-        var ts = [[4],[62],[100],[88],[70],[73],[22],[75],[29],[10]];
-        var ans = [0,0,1,1,0,0,0,1,0,0];
-        for i in 0...ts.count-1 {
-            var winner = topVotedCandidate.q(ts[i][0]);
+        let ts = [[4], [62], [100], [88], [70], [73], [22], [75], [29], [10]];
+        let ans = [0, 0, 1, 1, 0, 0, 0, 1, 0, 0];
+        for i in 0...ts.count - 1 {
+            let winner = topVotedCandidate.q(ts[i][0]);
             XCTAssertEqual(ans[i], winner);
         }
     }
 
     func test5() {
-        var persons = [0,1,2,3,0,3,4,1,4,2];
-        var times = [0,13,24,44,57,59,60,66,97,98];
-        var topVotedCandidate = TopVotedCandidate(persons, times);
+        let persons = [0, 1, 2, 3, 0, 3, 4, 1, 4, 2];
+        let times = [0, 13, 24, 44, 57, 59, 60, 66, 97, 98];
+        let topVotedCandidate = TopVotedCandidate(persons, times);
 
-        var ts = [[60],[98],[66],[96],[12],[14],[58],[97],[1],[65],[98],[67],[60],[58],[57],[24],[99],[13],[59],[23]];
-        var ans = [3,2,1,1,0,1,0,4,0,3,2,1,3,0,0,2,2,1,3,1];
-        for i in 0...ts.count-1 {
-            var winner = topVotedCandidate.q(ts[i][0]);
+        let ts = [[60], [98], [66], [96], [12], [14], [58], [97], [1], [65], [98], [67], [60], [58], [57], [24], [99], [13], [59], [23]];
+        let ans = [3, 2, 1, 1, 0, 1, 0, 4, 0, 3, 2, 1, 3, 0, 0, 2, 2, 1, 3, 1];
+        for i in 0...ts.count - 1 {
+            let winner = topVotedCandidate.q(ts[i][0]);
             XCTAssertEqual(ans[i], winner);
         }
     }
