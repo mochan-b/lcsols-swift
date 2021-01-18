7e6a36091bace38996e0e23c8fca3b0637425e9f
Mochan Shrestha <git@mochan.info>
2021-01-18 19:54:18 +0000

    Some cleanup of Solution 911

--- Sources/lcsols/Solution911.swift
+++ Sources/lcsols/Solution911.swift
@@ -1,57 +1,53 @@
 import Foundation
 
 public class TopVotedCandidate {
-    var timeVals: [Int];
-    var winners: [Int];
+    var timeVals: [Int]
+    var winners: [Int]
 
     public init(_ persons: [Int], _ times: [Int]) {
-        timeVals = times;
-        winners = [];
+        timeVals = times
+        winners = []
 
-        var first: Int = 0;
-        var firstname: Int = 0;
+        var first: Int = 0
+        var firstname: Int = 0
 
-        var votes: [Int: Int] = [:];
+        var votes: [Int: Int] = [:]
         for i in 0...persons.count - 1 {
-            let person = persons[i];
+            let person = persons[i]
 
             if (votes[person] == nil) {
-                votes[person] = 0;
+                votes[person] = 0
             }
 
-            let personVote = votes[person]! + 1;
-            votes[person] = personVote;
+            let personVote = votes[person]! + 1
+            votes[person] = personVote
 
             if (personVote >= first) {
-                first = personVote;
-                firstname = person;
+                first = personVote
+                firstname = person
             }
 
-            winners.append(firstname);
+            winners.append(firstname)
         }
     }
 
     public func q(_ t: Int) -> Int {
-        var left = 0, right = timeVals.count - 1, mid = 0;
-        var found = false;
+        var left = 0, right = timeVals.count - 1, mid = 0
+        var found = false
 
         while (left <= right) {
-            mid = (left + right) / 2;
+            mid = (left + right) / 2
 
             if (timeVals[mid] == t) {
-                found = true;
-                break;
+                found = true
+                break
             } else if (timeVals[mid] < t) {
-                left = mid + 1;
+                left = mid + 1
             } else {
                 right = mid - 1;
             }
         }
 
-        if (found) {
-            return winners[mid];
-        } else {
-            return winners[right];
-        }
+        return found ? winners[mid] : winners[right]
     }
 }
